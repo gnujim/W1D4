b8ce58b6441b633ffa5365e3e8922d867789f4c6
gnujim <mijung.m@gmail.com>
2017-09-07 19:42:18 +0000

    completed array mapping assignment

---
+++ array-mapping.js
@@ -0,0 +1,15 @@
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+var result = input.map(function pith(nums, idx, array) {
+  var ans = Math.sqrt(Math.pow(nums.x, 2) + Math.pow(nums.y, 2));
+  return ans;
+});
+
+console.log(result)
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
